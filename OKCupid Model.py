9b9de7a54d7f79713a011a8e4f5be23411e0f2da
alexdlilly <alexanderlilly.89@gmail.com>
2022-06-04 23:56:21 -0400

    test

--- OKCupid Model.py
+++ OKCupid Model.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 # what could be a fun thing to predict about people? Height? Income? To what degree they drink or smoke?
 data = pd.read_csv('profiles.csv')
 print(data.income.describe())
+print('hello')
 # Map "drinks" according to the following: not at all: 0, rarely: 1, socially: 2, often: 3, very often: 4
 data['drinks_mapped'] = data['drinks'].map({'not at all': 0, 'rarely': 1, 'socially': 2, 'often': 3, 'very often': 4})
 # Map 'smokes' according to the following: 'no': 0, 'when drinking': 1, 'sometimes': 2, 'trying to quit': 3, 'yes': 4
